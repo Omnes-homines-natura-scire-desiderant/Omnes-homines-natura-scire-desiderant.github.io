1b8fe1e5c07cf61d007b046b0d1a40884b00400b
Omnes-homines-natura-scire-desiderant <Ecalpala17@gmail.com>
2022-10-03 03:57:33 -0700

    Update to Website

--- Omni Fragrance Website 1.7/app.js
+++ Omni Fragrance Website 1.7/app.js
@@ -1,8 +1,3 @@
-// Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
-// slice extracts a section of a string without modifying original string
-//offsetTop - A Number, representing the top position of the element, in pixels
-
 // ********** set date ************
 // select span
 const date = document.getElementById("date");
@@ -49,6 +44,51 @@ window.addEventListener("scroll", function () {
     topLink.classList.remove("show-link");
   }
 });
+
+// ********** tab bars ************
+// 3 tab bars under about us section
+const about = document.querySelector(".about");
+const btns = document.querySelectorAll(".tab-btn");
+const articles = document.querySelectorAll(".content");
+about.addEventListener("click", function (e) {
+  const id = e.target.dataset.id;
+  if (id) {
+    // remove selected from other buttons
+    btns.forEach(function (btn) {
+      btn.classList.remove("active");
+    });
+    e.target.classList.add("active");
+    // hide other articles
+    articles.forEach(function (article) {
+      article.classList.remove("active");
+    });
+    const element = document.getElementById(id);
+    element.classList.add("active");
+  }
+});
+
+
+// ********** FAQ Questions ************
+// 3 questions
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
+
 
 // ********** smooth scroll ************
 // select links
